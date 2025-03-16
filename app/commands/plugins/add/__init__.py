669c61010c8c6ecc964f678f19d40f1dc7f86883
fa328 <fa328@njit.edu>
2025-03-16 19:49:57 -0400

    Update plugins folder

--- app/commands/plugins/add/__init__.py
+++ app/commands/plugins/add/__init__.py
@@ -1,7 +1,6 @@
 '''add funtion'''
 import sys
 import logging
-from app import App
 from app.commands import Command
 
 
@@ -13,8 +12,3 @@ class AddCommand(Command):
     def undo(self):
         '''Undo fuction'''
         logging.info('Undo the add')
-
-
-if __name__ == "__main__":
-    app = App()
-    app.start()
